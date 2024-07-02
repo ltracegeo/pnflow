cfeaacd6494066e897e5e9b317ecdb7a7e451315
Robim Pacheco <robim.pacheco@ltrace.com.br>
2024-07-02 19:05:44 -0300

    Update compile scripts for tests

--- teste/c/compile.sh
+++ teste/c/compile.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 ./clean.sh
-cp ../../build/win/src/pnm/pnflow/pnflow.lib .
-x86_64-w64-mingw32-g++ -static-libgcc test.cpp -L../../build/win/src/pnm/pnflow/ -I../../src/pnm/pnflow/ -lpnflow
+LIB_PATH=$(realpath ../../python/pnflow/)
+cp ${LIB_PATH}/pnflow.lib .
+x86_64-w64-mingw32-g++ -static-libgcc test.cpp -L${LIB_PATH} -I../../src/pnm/pnflow/ -lpnflow
--- teste/c/compile_linux.sh
+++ teste/c/compile_linux.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ./clean.sh
 # cp ../../build/linux/src/pnm/pnflow/libpnflow.so .
-LIB_PATH=$(realpath ../../build/linux/src/pnm/pnflow/)
-g++ -Wl,-rpath,${LIB_PATH} -static-libgcc test.cpp -L../../build/linux/src/pnm/pnflow/ -I../../src/pnm/pnflow/ -lpnflow
+LIB_PATH=$(realpath ../../python/pnflow/)
+g++ -Wl,-rpath,${LIB_PATH} -static-libgcc test.cpp -L../../python/pnflow/ -I../../src/pnm/pnflow/ -lpnflow
