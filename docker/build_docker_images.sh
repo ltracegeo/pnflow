29ad87be0d925d8d80cb81fbe5e6cd86fd982856
Robim Pacheco <robim.pacheco@ltrace.com.br>
2024-07-05 15:22:37 -0300

    Temporarily replace centos with ubuntu18 as docker image

--- docker/build_docker_images.sh
+++ docker/build_docker_images.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-docker build -t pnflow-build-linux docker/linux
+# docker build -t pnflow-build-linux docker/linux
+docker build -t pnflow-build-ubuntu18 docker/ubuntu18
 docker build -t pnflow-build-windows docker/windows
